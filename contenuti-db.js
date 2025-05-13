d48c59ee743e27be63efa2fda7e806d4d58ee78f
Daniel3720 <danielrazzante833@gmail.com>
2025-05-13 23:29:39 +0200

    contenuti-db.js

---
+++ contenuti-db.js
@@ -0,0 +1,26 @@
+const contenutiIndicizzati = [
+  {
+    titolo: "Il principio di legalità",
+    descrizione: "Analisi teorica e costituzionale del principio base del diritto penale.",
+    materia: "penale",
+    link: "approfondimenti.html"
+  },
+  {
+    titolo: "Simulazione contrattuale",
+    descrizione: "Conseguenze civili della simulazione assoluta e relativa.",
+    materia: "civile",
+    link: "contenuti.html"
+  },
+  {
+    titolo: "Motivazione del provvedimento",
+    descrizione: "Come redigere un atto amministrativo valido secondo la legge 241/1990.",
+    materia: "amministrativo",
+    link: "archivio.html"
+  },
+  {
+    titolo: "Responsabilità medica nella giurisprudenza",
+    descrizione: "Ultime sentenze civili della Cassazione.",
+    materia: "civile",
+    link: "contenuti.html"
+  }
+];

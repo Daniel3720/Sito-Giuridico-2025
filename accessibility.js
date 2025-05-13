6e36c5c6f2a6bd34cb165c803cd69013d13ac9a8
Daniel3720 <danielrazzante833@gmail.com>
2025-05-13 23:19:02 +0200

    accessibility.js

---
+++ accessibility.js
@@ -0,0 +1,11 @@
+function increaseFontSize() {
+  document.body.style.fontSize = "larger";
+}
+
+function decreaseFontSize() {
+  document.body.style.fontSize = "smaller";
+}
+
+function toggleContrast() {
+  document.body.classList.toggle("high-contrast");
+}
